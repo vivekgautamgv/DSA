f43852373bfd113ae1eb22d19ddd8e8a775bd31a
vivekgautamgv <gautamvivekgv@gmail.com>
2024-08-25 12:31:14 +0530

    hehe

--- java/sieve_algo.java
+++ java/sieve_algo.java
@@ -1,8 +1,8 @@
 package java;
 import java.util.*;
 import java.util.Scanner;
-public class sieve_algo {
-    public static void main(String args[]){
+public class sieve_algo{
+    public static void sieve_algo(String args[]){
         Scanner scanner  = new Scanner(System.in);
         int input  = scanner.nextInt();
 
