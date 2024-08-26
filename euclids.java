4f447f803c7e55820512d8280e89c3c6a4878dd1
vivekgautamgv <gautamvivekgv@gmail.com>
2024-08-26 09:29:02 +0530

    new changes

---
+++ euclids.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.Scanner;
+
+
+class maib {
+    public static int gcd(int a,int b){
+        if(a==0){
+            return b;
+        }
+
+        return gcd(b%a,a); 
+    }
+}
+public class euclids {
+
+    public static void main(String args[]){
+        
+        int n1 = 46;
+        int n2 = 23;
+        int result = maib.gcd(n1,n2);
+        System.out.println("GCD of "+n1+" and "+n2+" is "+result);
+
+    }
+    
+}
