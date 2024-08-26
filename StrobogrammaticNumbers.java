4cf9f3015778c27474429e281a2fbf477311f71f
vivekgautamgv <gautamvivekgv@gmail.com>
2024-08-26 11:55:51 +0530

    changes

---
+++ StrobogrammaticNumbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class stro{
+    static boolean isstro(String num){
+        Map<Character,Character> = new HashMap<Character,Character>();
+        map.put('6','9');
+        map.put('9','6');
+        map.put('8','8');
+        map.put('1','1');
+        map.put('0','0');
+
+        int l=0,r = num.length()-1;
+        while(l<=r){
+
+            if(!map.constrain Key(num.charAt(l),num.charAt(r))){
+
+    }
+}
